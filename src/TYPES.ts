a5f22be243d76cc20bf6053a0338a76642a6d367
Robert Klinger <robert@k1inger.de>
2021-10-13 17:28:31 +0200

    fix: added missing logConfig-Type

--- src/TYPES.ts
+++ src/TYPES.ts
@@ -2,5 +2,6 @@ export const BUGFINDER_COMMITPATH_LOCALITYPREPROCESSOR_COMMITSUBSET_TYPES = {
     skip:           Symbol("Skip"),
     n:              Symbol("n"),
     pathsHandling:  Symbol("PathHandling"),
-    logger:         Symbol("Logger")
+    logger:         Symbol("Logger"),
+    logConfig:      Symbol("LogConfig")
 }
